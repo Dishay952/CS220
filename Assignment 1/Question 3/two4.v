fe8ce81ca238e944653b26a20ffc584e4d90c77e
Aryan Vora <aryanv20@iitk.ac.in>
2022-01-19 00:21:44 +0530

    I wanna die

--- Assignment 1/Question 3/two4.v
+++ Assignment 1/Question 3/two4.v
@@ -9,8 +9,18 @@ module two_to_four(A1, A0, enable, Y3, Y2, Y1, Y0);
 input A1,A0,enable;
 output Y3,Y2,Y1,Y0;
 wire C3,C2,C1,C0;
-Y3<=A1&A0;
-Y2<=A1&(~A0);
-Y1<=A0&(~A1);
-Y0<=(~A1)&(~A0);
+always @(enable,A1,A0) begin
+if(enable==1) begin
+    Y3<=A1&A0;
+    Y2<=A1&(~A0);
+    Y1<=A0&(~A1);
+    Y0<=(~A1)&(~A0);
+end
+else
+    Y3<=1'b0;
+    Y2<=1'b0;
+    Y1<=1'b0;
+    Y0<=1'b0;
+end
+end
 endmodule
--- Assignment 1/Question 3/two4_tb.v
+++ Assignment 1/Question 3/two4_tb.v
@@ -9,18 +9,14 @@ two_to_four uut(A1,A0,enable,Y3,Y2,Y1,Y0);
 initial begin
     $dumpfile("two_to_four_tb.vcd");
     $dumpvars(0, two_to_four_tb);
-    A0<=1'b0;
-    A1<=1'b0;
-    enable<=1;
+    enable<=1'b1;
     $monitor("A1=%0b A0=%0b Enable=%0b Y3=%0b Y2=%0b Y1=%0b Y0=%0b", A1,A0,enable,Y3,Y2,Y1,Y0);
     #2000 $display("Test complete");
 end
-
 initial begin
-    //enable=1;
-    A0=0; A1=0;
-    #100 A0=1;A1=0;
-    #200 A0=0;A1=1;
-    #300 A0=1;A1=1;
+    #100 A0=1'b0; A1=1'b0;
+    #100 A0=1'b1;A1=1'b0;
+    #100 A0=1'b0;A1=1'b1;
+    #100 A0=1'b1;A1=1'b1;
 end
 endmodule 
