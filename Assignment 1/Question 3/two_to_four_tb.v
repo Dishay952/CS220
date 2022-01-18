713206ebf322b9d7e2e42cb6be559439065a2bd4
Aryan Vora <aryanv20@iitk.ac.in>
2022-01-18 17:30:53 +0530

    Q3 done successfully. Checked with GTKWave

--- Assignment 1/Question 3/two_to_four_tb.v
+++ Assignment 1/Question 3/two_to_four_tb.v
@@ -2,19 +2,21 @@
 `include "two_to_four.v"
 
 module two_to_four_tb;
-wire enable, A0, A1;
+reg enable, A0, A1;
 integer i;
 wire Y3,Y2,Y1,Y0;
 two_to_four uut(A1,A0,enable,Y3,Y2,Y1,Y0);
 initial begin
+    $dumpfile("two_to_four_tb.vcd");
+    $dumpvars(0, two_to_four_tb);
     A0<=0;
     A1<=0;
     enable<=0;
     $monitor("A1=%0b A0=%0b Enable=%0b Y3=%0b Y2=%0b Y1=%0b Y0=%0b", A1,A0,enable,Y3,Y2,Y1,Y0);
     for(i=0;i<8;i=i+1) begin
-        {enable,A1,A0}=i;
+        #5 {enable,A1,A0}<=i;
     end
-    $display("Test complete");
+    #50 $display("Test complete");
 end
 
 endmodule 
