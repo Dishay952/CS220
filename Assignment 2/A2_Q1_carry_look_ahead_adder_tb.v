57c39397402bdd22fd9f77606422f5cb4cc9525a
Tejas Ramakrishnan <82379532+tejasr20@users.noreply.github.com>
2022-02-05 17:51:34 +0530

    Updated test bench for 8 bit test cases

--- Assignment 2/A2_Q1_carry_look_ahead_adder_tb.v
+++ Assignment 2/A2_Q1_carry_look_ahead_adder_tb.v
@@ -1,16 +1,16 @@
 `timescale 1ns/1ns
 `include  "A2_Q1_carry_look_ahead_adder.v"
 module carry_look_ahead_adder_tb;
-reg [3:0] A;
-reg [3:0] B;
+reg [7:0] A;
+reg [7:0] B;
 reg C_in;
-wire [3:0] S;
+wire [7:0] S;
 // wire [3:0] P;
 // wire [3:0] C;
 wire C_out;
 integer i;
 integer j;
-carry_look_ahead_adder uut(C_out,S,A,B,C_in);
+A2_Q1_carry_look_ahead_adder uut(C_out,S,A,B,C_in);
 initial begin
     A<=4'b0;
     B<=4'b0;
@@ -19,7 +19,7 @@ initial begin
     $monitor("%t A=%b, B=%b, Carry=%b, Sum=%b",$time,A,B,C_out,S);
 end
 initial begin
-    for(i=14;i>0;i=i-1) begin 
+    for(i=255;i>0;i=i/2) begin 
         for(j=2;j<4;j=j+1) begin 
             #5 A<=i;B<=j;
         end
