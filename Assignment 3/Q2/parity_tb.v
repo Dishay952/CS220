f8fcea04aab4e7c02845a567fbbe2714968cf7ec
Tejas Ramakrishnan <82379532+tejasr20@users.noreply.github.com>
2022-02-17 17:25:59 +0530

    Update parity_tb.v

--- Assignment 3/Q2/parity_tb.v
+++ Assignment 3/Q2/parity_tb.v
@@ -4,40 +4,34 @@ module odd_parity_tb;
   // Ports
   wire p;
   reg ip,clk,reset;
-  reg [2:0] curr_state;
-  wire [2:0] next_state;
   odd_parity uut(clk,reset,ip,p);
   initial begin
       clk=1'b0;
-      #5;
-      $monitor($time," Clock=%b Reset=%b Input=%b Output=%b ",clk,reset,ip,p);
+      #1;
+      $monitor($time," Input=%b Output=%b ",ip,p);
       #15 $finish;
     end
     integer i;
   initial begin
     reset=1'b0;
-    #5;
-    ip=1'b0;
-    #5 
-    ip=1'b0;
-    #5 
-    ip=1'b0; ///000 should give parity bit output as one?
-    // #5 ip=1'b0;
-    // #5 ip=1'b1;
-    // #5 ip=1'b0;
-    // #5 ip=1'b1;
-    // #5 ip=1'b0;
-    // #5 ip=1'b1;
-    // #5 ip=1'b1;
-    // #5 ip=1'b0;
-    // #5 ip=1'b0;
-    // #5 ip=1'b1;
-    // #5 reset=1'b1;
-    // #5 ip=1'b0;
-    // #5 ip=1'b1;
-    // #5 ip=1'b0;
+    #1 ip=1'b0;
+    #1 ip=1'b0;
+    #1 ip=1'b1; //000 should give parity bit output as one
+    #1 ip=1'b1;
+    #1 ip=1'b1;
+    #1 ip=1'b0;//110 should give parity bit output as one
+    #1 ip=1'b1;
+    #1 ip=1'b0;
+    #1 ip=1'b1;//101 should give parity bit output as one
+    #1 ip=1'b0;
+    #1 ip=1'b1;
+    #1 ip=1'b1;//011 should give parity bit output as one
+    // #1 ip=1'b1;
+    #1 reset=1'b1;
+    #1 ip=1'b0;
+    #1 ip=1'b1;
+    #1 ip=1'b0;
     end
   always
-     #5 clk= ~clk ;
-
+     #1 clk= ~clk ;
 endmodule
