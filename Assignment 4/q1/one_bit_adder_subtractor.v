3d3f20cebf9df7193754ddc7abcf1fc7dee6f251
Tejas Ramakrishnan <82379532+tejasr20@users.noreply.github.com>
2022-03-02 00:19:21 +0530

    Delete Assignment 4/q1 directory

--- Assignment 4/q1/one_bit_adder_subtractor.v
+++
@@ -1,8 +0,0 @@
-module one_bit_adder_subtractor(C_out,S,A,B,C_in,opcode); //opcode 0 or +, 1 for -
-    input A,B,C_in,opcode;
-    output C_out;
-    output S;
-    //This is true for both addition and subtraction 
-    assign S=(A^(B^opcode))^C_in;
-    assign C_out=(A&(B^opcode))|((B^opcode)&C_in)|(C_in&A);
-endmodule
