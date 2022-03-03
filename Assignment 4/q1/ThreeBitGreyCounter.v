bc5006be8d8d5d4b561011057747881cd57f7a55
Dishay Mehta <dishaymehta952@gmail.com>
2022-03-03 19:32:51 +0530

    update

--- Assignment 4/q1/ThreeBitGreyCounter.v
+++ Assignment 4/q1/ThreeBitGreyCounter.v
@@ -1,5 +1,5 @@
-module ThreeBitGreyCounter(reset,out,A,count);
-input reset;
+module ThreeBitGreyCounter(clock,reset,out,A,count);
+input reset,clock;
 output reg out;
 output reg [2:0] A;
 output reg count;
@@ -13,68 +13,71 @@ parameter S4=3'b110;
 parameter S5=3'b111;
 parameter S6=3'b101;
 parameter S7=3'b100;
-always@(*) begin 
+always@(posedge clock or negedge clock) begin 
     if(reset) begin
         curr_state<=S0;
-        count<=0;
+        out<=0;
+        // count<=0;
     end
     else begin
         curr_state<=next_state;
     end
 end
-always@(curr_state) begin
+always@(curr_state or clock) begin
     case (curr_state)
         S0: begin
                 next_state<=S1;
                 out<=0;
-                A<=S1;
-                count<=1;
+                A=S1;
+                count=1;
         end
         S1: begin
                 next_state<=S2;
                 out<=0;
-                A<=S2;
-                count<=2;
+                A=S2;
+                count=2;
         end
         S2: begin
                 next_state<=S3;
                 out<=0;
-                A<=S3;
-                count<=3;
+                A=S3;
+                count=3;
         end
         S3: begin
                 next_state<=S4;
                 out<=0;
-                A<=S4;
-                count<=4;
+                A=S4;
+                count=4;
         end
         S4: begin
                 next_state<=S5;
                 out<=0;
-                A<=S5;
-                count<=5;
+                A=S5;
+                count=5;
         end
         S5: begin
                 next_state<=S6;
                 out<=0;
-                A<=S6;
-                count<=6;
+                A=S6;
+                count=6;
         end
         S6: begin
                 next_state<=S7;
                 out<=0;
-                A<=S7;
-                count<=7;
+                A=S7;
+                count=7;
         end
         S7: begin
                 next_state<=S0;
                 out<=1;
-                A<=S0;
-                count<=0;
+                A=S0;
+                count=0;
         end
         default: begin
                 next_state<=S0;
-                count<=0;
+                count=0;
+                out<=0;
+                A=S0;
         end
     endcase
 end
--- Assignment 4/q1/ThreeBitGreyCounter_tb.v
+++ Assignment 4/q1/ThreeBitGreyCounter_tb.v
@@ -1,16 +1,24 @@
 `timescale 1ns/1ns
 `include "ThreeBitGreyCounter.v"
 module ThreeBitGreyCounter_tb;
-  reg reset = 0;
+  // reg reset;
+  // reg out;
+  wire [2:0] A;
+  wire count;
+  reg reset,clock;
   wire out;
-  ThreeBitGreyCounter uut (reset,out,A,count);
-    initial begin
-        $monitor($time,"Input=%b Count=%b Grey_code=%b Output=%b",reset,count,A,out);
+  ThreeBitGreyCounter uut(clock,reset,out,A,count);
+  always 
+    #5 clock=~clock;
+  initial begin
+        clock=1'b0;
+        #1;
+        $monitor($time," Input=%b Count=%b Grey_code=%b Output=%b",reset,count,A,out);
         #120 $finish;
-      end
+  end
     initial begin
       reset=1'b0;
-      #40 reset=1'b1;
-      #20 reset=1'b0;
+      // A=3'b000;
+      // count=0;
       end
 endmodule
