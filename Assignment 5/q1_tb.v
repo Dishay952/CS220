f9c8fe4067076290cd21d15640e48f4d4bc172c7
Aryan Vora <aryanv20@iitk.ac.in>
2022-03-18 12:28:04 +0530

    q1 done

--- Assignment 5/q1_tb.v
+++ Assignment 5/q1_tb.v
@@ -5,21 +5,43 @@ reg [7:0] a;
 reg [7:0] b;
 wire [7:0] ans;
 integer i;
-reg clk;
-gcd uut(ans,clk,a,b);
+gcd uut(ans,a,b);
 initial begin
-  a<=6;
-  b<=3;
-  for(i=0;i<=15;i=i+1) begin
-      #5 $display("A=%d B=%d GCD=%d",a,b,ans);
-      a=a+1;
-      b=b+1;
-  end
+  #5 a=4;
+  #5 a=5;
+  #5 a=8;
+  #5 a=14;
+  #5 a=67;
+  #5 a=144;
+  #5 a=35;
+  #5 a=70;
+  #5 a=40;
+  #5 a=16;
+  #5 a=20;
+  #5 a=10;
+  #5 a=45;
+  #5 a=18;
+  #5 a=27;
+end
+initial begin 
+  #5 b=3;
+  #5 b=15;
+  #5 b=12;
+  #5 b=43;
+  #5 b=18;
+  #5 b=60;
+  #5 b=14;
+  #5 b=45;
+  #5 b=24;
+  #5 b=18;
+  #5 b=87;
+  #5 b=15;
+  #5 b=60;
+  #5 b=54;
+  #5 b=45;
 end
 initial begin
-  #1 clk=~clk;
-end
-initial begin
+  $monitor("A=%d B=%d GCD=%d",a,b,ans);
   #180 $finish;
 end
 endmodule
